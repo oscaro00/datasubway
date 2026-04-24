fcd45f2190ecac93939af781b40618707ccaed60
oscaro00 <obrien.oscar2000@gmail.com>
2026-04-24 17:53:28 -0400

    clean slate with polars

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,1 @@
 pub mod data_model;
-pub mod model;
-pub mod optimizer;
--- src/model/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod column_context;
-pub mod combine_measures;
-pub mod data_sources;
-pub mod filter_tree;
-pub mod joins;
-pub mod pre_agg;
-pub mod query_context;
